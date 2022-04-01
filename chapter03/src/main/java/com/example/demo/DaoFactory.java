9fbbd47a061440e0fd330d724e7ca54941832e41
kingjaewon2000 <kkkxx_00@naver.com>
2022-04-02 00:46:56 +0900

    chapter05 init

--- chapter03/src/main/java/com/example/demo/DaoFactory.java
+++ chapter03/src/main/java/com/example/demo/DaoFactory.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 import javax.sql.DataSource;
 
+@Controller
 @Configuration
 public class DaoFactory {
 
